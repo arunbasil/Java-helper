1fab50256fb5436314517d3df4b08e44487b41b3
arunbasil <arunbasil83@gmail.com>
2024-09-25 19:28:55 +1200

    Update SchemaUtils.java

--- SchemaUtils.java
+++ SchemaUtils.java
@@ -16,25 +16,33 @@ public class SchemaUtils {
     public static Map<String, String> parseYamlSpec(String yamlFilePath) throws Exception {
         Map<String, String> fields = new HashMap<>();
         Yaml yaml = new Yaml();
+
         try (InputStream inputStream = new FileInputStream(yamlFilePath)) {
-            Map<String, Object> yamlContent = yaml.load(inputStream);
-            Map<String, Object> components = (Map<String, Object>) yamlContent.get("components");
-            if (components != null) {
-                Map<String, Object> schemas = (Map<String, Object>) components.get("schemas");
-                if (schemas != null) {
-                    for (Map.Entry<String, Object> entry : schemas.entrySet()) {
-                        Map<String, Object> schema = (Map<String, Object>) entry.getValue();
-                        Map<String, Object> properties = (Map<String, Object>) schema.get("properties");
-                        if (properties != null) {
-                            for (Map.Entry<String, Object> property : properties.entrySet()) {
-                                String fieldName = property.getKey();
-                                Map<String, Object> fieldDetails = (Map<String, Object>) property.getValue();
-                                String fieldType = (String) fieldDetails.get("type");
-                                fields.put(fieldName, fieldType);
+            Object yamlContentObj = yaml.load(inputStream);
+            if (yamlContentObj instanceof Map) {
+                Map<String, Object> yamlContent = (Map<String, Object>) yamlContentObj;
+                Map<String, Object> components = (Map<String, Object>) yamlContent.get("components");
+
+                if (components != null) {
+                    Map<String, Object> schemas = (Map<String, Object>) components.get("schemas");
+                    if (schemas != null) {
+                        for (Map.Entry<String, Object> entry : schemas.entrySet()) {
+                            Map<String, Object> schema = (Map<String, Object>) entry.getValue();
+                            Map<String, Object> properties = (Map<String, Object>) schema.get("properties");
+
+                            if (properties != null) {
+                                for (Map.Entry<String, Object> property : properties.entrySet()) {
+                                    String fieldName = property.getKey();
+                                    Map<String, Object> fieldDetails = (Map<String, Object>) property.getValue();
+                                    String fieldType = (String) fieldDetails.get("type");
+                                    fields.put(fieldName, fieldType);
+                                }
                             }
                         }
                     }
                 }
+            } else {
+                System.out.println("Invalid YAML structure. Expected a map.");
             }
         }
         return fields;
@@ -45,6 +53,7 @@ public class SchemaUtils {
         Map<String, String> fields = new HashMap<>();
         Schema schema = new Parser().parse(new FileInputStream(avroFilePath));
         List<Schema.Field> avroFields = schema.getFields();
+
         for (Schema.Field field : avroFields) {
             fields.put(field.name(), field.schema().getType().getName());
         }
